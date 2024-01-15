132d943fb429a03e1a0fd15b82d0254a77d68949
Léo Brito <Leonardobrito371@gmail.com>
2024-01-15 22:12:39 +0000

    hello World

---
+++ teste.helloWorld/Produto/Entidades/Produto.cs
@@ -0,0 +1,12 @@
+namespace teste.helloWorld.Produto.Entidades;
+
+public class Produto
+{
+    public string nome;
+    public int quantidade;
+
+    public Produto(string name, int qnt){
+        this.nome = name;
+        this.quantidade = qnt;
+    }
+}
---
+++ teste.helloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿using teste.helloWorld.Produto.Entidades;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Produto pd = new Produto("Geladeira", 10);
+Console.WriteLine(pd.nome);
